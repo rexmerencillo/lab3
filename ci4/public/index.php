dacd966cbe95dc580933766b4526f1008662947d
rexmerencillo <jbmerencillo@student.apc.edu.ph>
2023-03-07 20:14:39 +0800

    index.html

--- ci4/public/index.php
+++ ci4/public/index.php
@@ -132,6 +132,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
 ?>
 </div>
+<style>
+body {
+  background-image: url('https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&w=1000&q=80');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
+</head>
+<body>
+
+<h2>WELCOME TO MY LIFE</h2>
+
+<table style="width:100%">
+
+    <tr>
+      <td>name:</td>
+      <td>section:</td>
+      <td>course:</td>
+    </tr>
+    <tr>
+      <td>Johnrexmerencillo</td>
+      <td>ss211</td>
+      <td>BSCS-SS</td>
+    </tr>
+  </table>
+  <br>
+  <br>
+
+  <p id="demo">IF YOU WANT TO KNOW MORE ABOUT ME CLICK THE BUTTOM .</p>
+
+  <button type="button" onclick='document.getElementById("demo").innerHTML = "Hi I am Johnrexmerencillo " ' >Click Me!</button>
 
 
 </body>
